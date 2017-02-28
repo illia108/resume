22fdae88b32832c79693c7e72e3a7d6484d046b1
illia108 <illia.kuzma@gmail.com>
2017-02-28 20:31:17 +0200

    Show Levels when user scrolls all the way to the bottom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 $(function(){
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      showLevels();
+    }
+});
+  
+});
+
+function showLevels() {
   $('.level-bar-inner').each(function() {
     var itemWidth = $(this).data('level');
     $(this).animate({
         width: itemWidth
     }, 800);
   });
-});
+}
